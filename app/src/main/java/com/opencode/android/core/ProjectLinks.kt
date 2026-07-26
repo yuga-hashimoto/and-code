da18ffa332bd08899a822cd750f05bbec63fa19a
Yu-ga <74749461+yuga-hashimoto@users.noreply.github.com>
2026-07-26 21:24:56 +0900

    feat: centralize project links

---
+++ app/src/main/java/com/opencode/android/core/ProjectLinks.kt
@@ -0,0 +1,8 @@
+package com.opencode.android.core
+
+object ProjectLinks {
+    const val GITHUB_REPOSITORY = "https://github.com/yuga-hashimoto/opencode-android"
+    const val GITHUB_ISSUES = "$GITHUB_REPOSITORY/issues"
+    const val GITHUB_RELEASES = "$GITHUB_REPOSITORY/releases"
+    const val LICENSE = "$GITHUB_REPOSITORY/blob/main/LICENSE"
+}
