99b27264e6c1fc5d7d21b067085663a3f8d2bb9f
Yu-ga <74749461+yuga-hashimoto@users.noreply.github.com>
2026-08-04 23:16:25 +0000

    test: add scratch file to verify keyless OCR review bot

---
+++ app/src/main/java/com/yugahashimoto/andcode/OcrFreeTest.kt
@@ -0,0 +1,11 @@
+package com.yugahashimoto.andcode
+
+class OcrFreeTest {
+    fun lengthOf(text: String?): Int {
+        return text!!.length
+    }
+
+    fun firstChar(items: List<String>?): Char {
+        return items!!.first()[0]
+    }
+}
