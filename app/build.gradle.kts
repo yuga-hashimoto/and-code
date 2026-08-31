dc8a1f35bca48477b0bcf9602a139461e0a568f9
Yu-ga <74749461+yuga-hashimoto@users.noreply.github.com>
2026-08-31 03:20:25 +0000

    chore(release): prepare v1.2.14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -103,8 +103,8 @@ android {
         applicationId = "com.yugahashimoto.andcode"
         minSdk = 26
         targetSdk = 35
-        versionCode = 52
-        versionName = "1.2.13"
+        versionCode = 53
+        versionName = "1.2.14"
         buildConfigField("String", "GITHUB_CLIENT_ID", "\"$githubClientId\"")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
